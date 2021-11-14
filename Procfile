05e1b3fde8817935c0aac721bfcf32e3354a743e
Bilal-Zafar786 <bzcode786@gmail.com>
2021-11-14 18:08:46 +0500

    1st Deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn ddtestApp.wsgi --log-file=-
---
+++ ddtestApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DdtestappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ddtestApp'
